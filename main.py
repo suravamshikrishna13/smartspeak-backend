363ea584e39dab0b8d937542e9f57d2c776800a7
sura vamshi krishna <suravamshikrishna13@gmail.com>
2026-01-24 18:28:14 +0530

    Add root endpoint

--- main.py
+++ main.py
@@ -1,4 +1,11 @@
+
 from fastapi import FastAPI
+
+app = FastAPI()
+
+@app.get("/")
+def root():
+    return {"status": "SmartSpeak backend running"}
 from fastapi.middleware.cors import CORSMiddleware
 from pydantic import BaseModel
 
